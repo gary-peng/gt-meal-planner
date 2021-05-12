aa13f573c8016925d32afeed8c2097ede059f461
Gary Peng <49541146+gary-peng@users.noreply.github.com>
2021-05-11 21:03:06 -0700

    Added sample menu

--- routes/menu-controller.js
+++ routes/menu-controller.js
@@ -3,7 +3,19 @@ const fs = require("fs");
 function getMenu(req, res, meal) {
     fs.readFile("scraping/" + meal + ".json", (err, data) => {
         if (err) throw err;
+
         let menu = JSON.parse(data);
+        if (menu[0] == null) {
+            fs.readFile("scraping/sample/" + meal + ".json", (err, data) => {
+                if (err) throw err;
+        
+                let menu = JSON.parse(data);
+                res.json(menu);
+            });
+
+            return;
+        }
+
         res.json(menu);
     });
 }
